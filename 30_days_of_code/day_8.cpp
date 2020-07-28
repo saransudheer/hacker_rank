72c6366bc350dc0817f2e854945aa68777535f29
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-07-28 23:11:57 +0530

    Create day_8.cpp

---
+++ 30_days_of_code/day_8.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+#include <vector>
+
+#include <string>
+#include <map>
+using namespace std;
+
+
+
+
+
+int main() {
+
+    int x;
+
+    cin >> x;
+
+    map<string, string> all;
+
+    for(int i = 0; i < x; ++i){
+
+        string s, p;
+
+        cin >> s >> p;
+
+        all[s] = p;
+
+    }
+
+    string h;
+
+    while(cin >> h){
+
+        if(all.find(h) == all.end()){
+
+            cout << "Not found" << endl;
+
+        }
+
+        else{
+
+            cout << h << "=" << all.at(h) << endl;
+
+        }
+
+    }
+
+}
