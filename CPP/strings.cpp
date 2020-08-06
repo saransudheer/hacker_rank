da2c8da05627475a67ca2fd251db029c0ae31601
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-08-06 12:25:29 +0530

    Create strings.cpp

---
+++ CPP/strings.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a;
+    string b;
+    string e;
+    int i,j;
+    //cin.ignore();
+    getline(cin,a);
+    getline(cin, b);
+    cout<<a.length()<<" "<<b.length();
+    cout<<"\n"<<a+b;
+    e = a[]
+    return 0;
+}
