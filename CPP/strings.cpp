66f29ffa6299dcacccdfd00abf2c6b79964892b4
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-08-09 23:38:21 +0530

    Update strings.cpp

--- CPP/strings.cpp
+++ CPP/strings.cpp
@@ -6,13 +6,18 @@ int main() {
 	// Complete the program
     string a;
     string b;
-    string e;
-    int i,j;
-    //cin.ignore();
+    /*cin.ignore();
     getline(cin,a);
-    getline(cin, b);
-    cout<<a.length()<<" "<<b.length();
-    cout<<"\n"<<a+b;
-    e = a[]
+    getline(cin,b); 
+    */
+    cin>>a>>b;
+    cout<<a.size()<<" "<<b.size()<<endl;
+    cout<<a+b<<endl;
+    char temp=a[0];
+    char temp1=b[0];
+    a[0]=temp1;
+    b[0]=temp;
+    cout<<a<<" "<<b;
+	
     return 0;
 }
