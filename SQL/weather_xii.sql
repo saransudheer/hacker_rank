12c2ac252ed5b9ee4947623c0cd538d0aab43121
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-08-26 23:05:48 +0530

    Adding weather_xii

---
+++ SQL/weather_xii.sql
@@ -0,0 +1,5 @@
+/*
+Enter your query here.
+*/select distinct city from STATION where not (city like 'a%' or city like 'e%' or city like 'i%' or city like 'o%' or city like 'u%') 
+and not
+(city like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u' )order by city;
