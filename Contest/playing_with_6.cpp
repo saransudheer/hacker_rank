2d7bb0ac52bc885020cf3edff727338c628070fe
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-09-02 14:45:57 +0530

    Create playing_with_6.cpp

---
+++ Contest/playing_with_6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+	int b,k,n=0;
+	cin>>b>>k;
+	int c[k];
+	for(int i=0; i<k; i++){
+		cin>>c[i];
+	}
+	for(int i=0; i<k; i++){
+		n += pow((c[i]*b),k-1);
+	}
+	if(n%2==0){
+		cout<<"even";
+	}
+	else{
+		cout<<"odd";
+	}
+	return 0;
+}
