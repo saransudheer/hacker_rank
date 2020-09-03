25e5f34636c61f0bb90192e43b54b043011a9bf7
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-09-03 12:14:40 +0530

    Done small

---
+++ Contest/small.cpp
@@ -0,0 +1,43 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int m, s, t, i;
+    cin >> m >> s;
+    string a, b;
+
+    if (s == 0)
+    {
+        cout << (m == 1 ? "0 0" : "-1 -1") << endl;
+        return 0;
+    }
+
+    for (i = 0; i < m; i++)
+    {
+        t = min(s, 9);
+        b += t + '0';
+        s -= t;
+    }
+
+    if (s > 0)
+    {
+        cout << "-1 -1" << endl;
+        return 0;
+    }
+
+    for (i = m - 1; i >= 0; i--)
+        a += b[i];
+
+    for (i = 0; a[i] == '0'; i++);
+
+    a[i]--, a[0]++;
+    cout << a << " " << b << endl;
+    return 0;
+}
