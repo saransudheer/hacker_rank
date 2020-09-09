04e071d059e1d46f3d92d153900894c0dc07396a
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-09-09 22:45:33 +0530

    In progress

---
+++ Contest/rankk_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    int a[n], b[n], c[n], d[n], count[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i]>>b[i]>>c[i]>>d[i];
+        if(a[i]>b[i]&&a[i]>c[i]&&a[i]>d[i]){
+            count[i]=i;
+        }
+        else if (condition)
+    }
+    
+    
+    return 0;
+}
