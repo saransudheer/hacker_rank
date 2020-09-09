d97377a2db9bb2f9c14aeb20a3ba3b2e22e9df93
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-09-09 23:24:42 +0530

    Errors exist

--- Contest/rankk_1.cpp
+++ Contest/rankk_1.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
+int MAX_INT(int arr[],int s)
+{
+    return max_element(arr, arr + s) - arr;
+}
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int n;
     cin>>n;
-    int a[n], b[n], c[n], d[n], count=0, count1=0, count2=0, count3=0;
+    int a[n], b[n], c[n], d[n], sum[n];
     for(int i=0; i<n; i++){
         cin>>a[i]>>b[i]>>c[i]>>d[i];
-        if(a[i]>b[i]&&a[i]>c[i]&&a[i]>d[i]){
-            count+=1;
-        }
-        else if (b[i]>c[i]&&b[i]>d[i]){
-            count1+=1;
-        }
-        else if (c[i]>d[i]){
-            count2 +=1;
-        }
-        else{
-            count3 +=1;
-        }
-    }
+        sum[i]=a[i]+b[i]+c[i]+d[i];        
     
-    
+    }    
+    cout<<MAX_INT(sum,n)+1;
     return 0;
 }
