81f7ba7340048842eb033a09b22b4006985a9e11
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-09-09 22:53:48 +0530

    In progress

--- Contest/rankk_1.cpp
+++ Contest/rankk_1.cpp
@@ -7,13 +7,21 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int n;
     cin>>n;
-    int a[n], b[n], c[n], d[n], count[n];
+    int a[n], b[n], c[n], d[n], count=0, count1=0, count2=0, count3=0;
     for(int i=0; i<n; i++){
         cin>>a[i]>>b[i]>>c[i]>>d[i];
         if(a[i]>b[i]&&a[i]>c[i]&&a[i]>d[i]){
-            count[i]=i;
+            count+=1;
         }
-        else if (condition)
+        else if (b[i]>c[i]&&b[i]>d[i]){
+            count1+=1;
+        }
+        else if (c[i]>d[i]){
+            count2 +=1;
+        }
+        else{
+            count3 +=1;
+        }
     }
     
     
