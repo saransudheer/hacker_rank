00437f7683e6963450696fdc7f71fa95868e14aa
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-09-10 12:23:09 +0530

    In progress....

--- Contest/co_prime.cpp
+++ Contest/co_prime.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
-typedef long long int lli;
-bool is_prime(int n) {
+typedef long int lli;
+bool is_prime(lli n) {
     if (n == 1) {
         return false;
     }
-    int i = 2;
+    lli i = 2;
     while (i*i <= n) {        
         if (n % i == 0) {
             return false;
@@ -15,16 +16,26 @@ bool is_prime(int n) {
     }
     return true;
 }
-void printPrime(int n) 
+lli countprime(lli n) 
 { 
-    for (int i = 2; i <= n; i++) { 
-        if (isPrime(i)) 
-            cout << i << " "; 
+    vector<lli>a;
+    for (lli i = 2; i <= n; i++) { 
+        if (is_prime(i)) 
+        {
+            a.push_back(i);
+        }
     } 
+    return a.size();
 } 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-   
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        lli b;
+        cin>>b;
+        cout<<countprime(b);     
+    }
 
     
     return 0;
