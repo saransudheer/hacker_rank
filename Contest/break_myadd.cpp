2dbd07f6a55f2c1f1af5877f7ed77fb8fb9f986a
Saran Sudheer <56349474+saransudheer@users.noreply.github.com>
2020-09-18 00:44:05 +0530

    In progress

--- Contest/break_myadd.cpp
+++ Contest/break_myadd.cpp
@@ -1,26 +1,14 @@
-
 #include <cstdio>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 typedef long long int lli;
-bool isprime(lli a){
-    int i=2;
-    while(i<=a/2){
-        if(a%i==0){
-            return false;
-            exit(0);
-        }
-        i++;
-    }
-    return true;
-}
 lli solve(lli a){
-    int sum=a, item=a/2;
+    lli sum=a, item=a/2;
     if (a==1){
         return 1;
     }
-    else if(isprime(a)==true && a!=1){
+    else if(a%2!=0 && a!=1){
         return a+1;
         exit(0);
     }
